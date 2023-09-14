b9e6d6c63890d0243d687f97b0f799013db5e68f
sara <sara1992@live.com.pt>
2023-09-14 15:20:44 +0200

    filters js

--- base.js
+++ base.js
@@ -3,74 +3,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Récupérez les données depuis le backend
   fetch("http://localhost:5678/api/works")
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error("Échec de la récupération des données");
       }
       return response.json();
     })
-    .then(responseData => {
-      data = responseData; // Stock les données dans la variable data
+    .then((responseData) => {
+      data = responseData; // Stockez les données dans la variable data
+      generationProjets(data, null); // Appelez la fonction pour générer les projets initiaux
 
-      // Appelez la fonction pour générer les projets initiaux
-      generationProjets(data, null);
+      const tousBtn = document.querySelector(".tousBtn");
+      tousBtn.addEventListener("click", () => generationProjets(data, null)); // Utilisez une fonction fléchée ici
 
-      //EventListener sur les boutons de filtre
-     // Créez les boutons de filtre
 
-            const btnAll = document.createElement("button");
-            btnAll.className = "filter__btn filter__btn-id-null";
-            btnAll.textContent = "Tous";
-          
-            const btnId1 = document.createElement("button");
-            btnId1.className = "filter__btn filter__btn-id-1";
-            btnId1.textContent = "Objets";
-          
-            const btnId2 = document.createElement("button");
-            btnId2.className = "filter__btn filter__btn-id-2";
-            btnId2.textContent = "Appartements";
-          
-            const btnId3 = document.createElement("button");
-            btnId3.className = "filter__btn filter__btn-id-3";
-            btnId3.textContent = "Hôtels & restaurants";
-          
-            // Ajoutez les boutons de filtre à l'élément conteneur
-            filterContainer.appendChild(btnAll);
-            filterContainer.appendChild(btnId1);
-            filterContainer.appendChild(btnId2);
-            filterContainer.appendChild(btnId3);
-    
-    
-    
-    
-    
-
-      btnAll.addEventListener("click", () => { // Tous les projets
-        generationProjets(data, null);
-      });
-
-      btnId1.addEventListener("click", () => { // Objets
-        generationProjets(data, 1);
-      });
-
-      btnId2.addEventListener("click", () => { // Appartements
-        generationProjets(data, 2);
-      });
-
-      btnId3.addEventListener("click", () => { // Hôtels & restaurants
-        generationProjets(data, 3);
-      });
-    })
-    .catch(error => console.log(error));
+    //  buttons 
+      fetch("http://localhost:5678/api/categories")
+        .then((resCategories) => {
+          return resCategories.json();
+        })
+        .then((dataCategories) => {
+          dataCategories.forEach((category) => {
+            const container = document.querySelector('.filters'); // Vous avez manqué un point ici
+            let btn = document.createElement("button");
+            btn.classList.add("btn");
+            
+            btn.textContent = category.name;
+            btn.addEventListener("click", () => generationProjets(data, category.id)); // Utilisez une fonction fléchée ici
+            container.appendChild(btn);
+          });
+        });
+    });
 
   // Fonction pour générer les projets en fonction du filtre
   function generationProjets(data, id) {
     const galleryContainer = document.querySelector(".gallery");
-    galleryContainer.innerHTML = ''; // Effacez la galerie existante
+    galleryContainer.innerHTML = ""; // Effacez la galerie existante
 
     if (id === null) {
       // Affichez tous les projets
-      data.forEach(work => {
+      data.forEach((work) => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -85,8 +57,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     } else {
       // Affichez les projets filtrés
-      const filteredData = data.filter(work => work.categoryId === id);
-      filteredData.forEach(work => {
+      const filteredData = data.filter((work) => work.categoryId === id);
+      filteredData.forEach((work) => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -102,5 +74,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
-
-
