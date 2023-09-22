254984af5ad0e3cf9a3c4b3ad3df15a40464d279
sara <sara1992@live.com.pt>
2023-09-22 18:51:15 +0200

    start admin

--- FrontEnd/script/base.js
+++ FrontEnd/script/base.js
@@ -99,6 +99,24 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+const token = sessionStorage.getItem("token");
+const AlredyLogged = document.querySelector("#login");
+
+adminPanel()
+// Gestion de l'affichage des boutons admin
+function adminPanel() {
+    document.querySelectorAll(".admin__modifer").forEach(a => {
+        if (token === null) {
+            return;
+        }
+        else {
+            a.removeAttribute("aria-hidden")
+            a.removeAttribute("style")
+            AlredyLogged.innerHTML = "deconnexion";
+        }
+    });
+}
+
 
 
 
